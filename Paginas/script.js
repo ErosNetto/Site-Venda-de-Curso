f4999b3db483ade54a7eef191e1f2552bdee97d7
Eros Netto Antunes <erosnetto1002@gmail.com>
2023-04-22 21:25:51 -0300

    Form de cadastro v1 feito

---
+++ Paginas/script.js
@@ -0,0 +1,21 @@
+// Background de uma cor
+let menuItem = document.querySelectorAll('.item-menu');
+function selectLink() {
+    menuItem.forEach(item => {
+        item.classList.remove('ativo');
+    });
+
+    this.classList.add('ativo');
+}
+menuItem.forEach(item => {
+    item.addEventListener('click', selectLink);
+});
+
+
+// Expandir menu
+const btnEx = document.querySelector('#btn-ex');
+const menuLateral = document.querySelector('.menu-lateral');
+
+btnEx.addEventListener('click', () => {
+    menuLateral.classList.toggle('expandir');
+});

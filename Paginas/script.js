2ca94b97a00bc2eb7071fe4415f6dc1ec4a7a8af
Eros Netto Antunes <erosnetto1002@gmail.com>
2023-04-27 23:51:40 -0300

    pagInicial

--- Paginas/script.js
+++ Paginas/script.js
@@ -1,15 +1,15 @@
-// Background de uma cor
-let menuItem = document.querySelectorAll('.item-menu');
-function selectLink() {
-    menuItem.forEach(item => {
-        item.classList.remove('ativo');
-    });
+// // Background de uma cor
+// let menuItem = document.querySelectorAll('.item-menu');
+// function selectLink() {
+//     menuItem.forEach(item => {
+//         item.classList.remove('ativo');
+//     });
 
-    this.classList.add('ativo');
-}
-menuItem.forEach(item => {
-    item.addEventListener('click', selectLink);
-});
+//     this.classList.add('ativo');
+// }
+// menuItem.forEach(item => {
+//     item.addEventListener('click', selectLink);
+// });
 
 
 // Expandir menu

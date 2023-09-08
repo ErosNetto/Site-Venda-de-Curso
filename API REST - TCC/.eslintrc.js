19edd45b69d04a5657e8a6c596db1bee7f4ed138
Eros Netto <erosnetto1002@gmail.com>
2023-09-08 10:26:40 -0300

    .

--- API REST - TCC/.eslintrc.js
+++ API REST - TCC/.eslintrc.js
@@ -3,18 +3,18 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: 'airbnb-base',
-  overrides: [
-  ],
+  extends: "airbnb-base",
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
   rules: {
-    'no-console': 'off',
-    'class-methods-use-this': 'off',
-    'import/first': 'off',
-    'no-param-reassign': 'off',
-    camelcase: 'off',
+    "no-console": "off",
+    "class-methods-use-this": "off",
+    "import/first": "off",
+    "no-param-reassign": "off",
+    camelcase: "off",
+    quotes: "off",
   },
 };
